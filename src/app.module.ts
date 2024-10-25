08279d35afe4820893dcec81cdaba13ff5b8978f
xu yaxiong <xyxlindy@163.com>
2024-10-26 00:00:31 +0800

    添加配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import localConfig from './config/local.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeasureController } from './measure/measure.controller';
 import { MeasureService } from './measure/measure.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [localConfig] })],
   controllers: [AppController, MeasureController],
   providers: [AppService, MeasureService],
 })
--- src/measure/measure.service.ts
+++ src/measure/measure.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MeasureService {
+  constructor(private readonly configService: ConfigService) {}
   measure(): string {
     return 'measure';
   }
