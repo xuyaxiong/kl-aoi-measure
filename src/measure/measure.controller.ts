b7e924c7b199c5d39cc832fc32f6ef1e0e190a49
xuyaxiong <xyxlindy@163.com>
2024-11-14 15:27:19 +0800

    更新文档

--- src/measure/measure.controller.ts
+++ src/measure/measure.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MeasureService } from './measure.service';
 import { MeasureParam } from './measure.param';
 import HttpResponse from 'src/utils/api_res';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('测量')
 @Controller('measure')
 export class MeasureController {
   constructor(private readonly measureService: MeasureService) {}
 
   @Post('measure')
+  @ApiOperation({ summary: '测量接口' })
   async measure(@Body() measureParam: MeasureParam) {
     try {
       const res = await this.measureService.measure(measureParam);
