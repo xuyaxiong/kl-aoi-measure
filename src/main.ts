912ad882951e56f56884a7e68ab23fb6512e677f
xuyaxiong <xyxlindy@163.com>
2024-11-20 14:22:49 +0800

    1. 添加winston日志配置； 2. 添加slogan。

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,36 @@ const path = require('path');
 import { LoggingInterceptor } from './interceptor/logging.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import AppConfig from './app.config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: configLogger(),
+  });
   app.useGlobalInterceptors(new LoggingInterceptor());
   swaggerDoc(app);
   dllDump(process.pid);
   await app.listen(process.env.PORT ?? 3100);
+  slogan('AOI MEASURE');
 }
 
 bootstrap();
+
+function slogan(text: string) {
+  console.log(
+    chalk.blue(
+      figlet.textSync(text, {
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 100,
+      }),
+    ),
+  );
+}
 
 function swaggerDoc(app) {
   const config = new DocumentBuilder()
@@ -47,5 +67,39 @@ function dllDump(port: number) {
 
   procdump.on('exit', (code) => {
     console.log('procdump_exit', `Child exited with code ${code}`);
+  });
+}
+
+function configLogger() {
+  const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
+    dirname: AppConfig.APP_LOG_DIR,
+    filename: 'application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.uncolorize(),
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      winston.format.printf(({ timestamp, level, message, context }) => {
+        return `[${timestamp}] ${level.toUpperCase()} [${context || 'App'}]: ${message}`;
+      }),
+    ),
+  });
+  return WinstonModule.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      winston.format.printf(({ level, message, timestamp, context }) => {
+        const formatContext = chalk.yellow(`[${context}]`);
+        return `[${timestamp}] ${level} ${formatContext}: ${message}`;
+      }),
+    ),
+    transports: [dailyRotateFileTransport, new winston.transports.Console()],
   });
 }
