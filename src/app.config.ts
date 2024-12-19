527dc4e969e572d3bef916cf77c5b81fa0f28572
xuyaxiong <xyxlindy@163.com>
2024-12-19 15:48:36 +0800

    修改配置文件

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,11 @@
+const path = require('path');
+
+const KL_STORAGE_PATH = 'D:\\kl-storage';
+
 const AppConfig = {
-  DLL_PATH: 'D:\\kl-storage\\dll\\', // dll路径
-  CRASH_DUMP_DIR: 'D:\\kl-storage\\crashDump\\aoi-measure', // dll崩溃dump文件保存路径
-  APP_LOG_DIR: 'D:\\kl-storage\\app-logs\\aoi-measure', // 服务日志保存路径
+  DLL_PATH: path.join(KL_STORAGE_PATH, 'dll'), // dll路径
+  CRASH_DUMP_DIR: path.join(KL_STORAGE_PATH, 'crashDump', 'aoi-measure'), // dll崩溃dump文件保存路径
+  APP_LOG_DIR: path.join(KL_STORAGE_PATH, 'app-logs', 'aoi-measure'), // 服务日志保存路径
   TMP_DIR: 'D:\\tmp', // 临时文件夹
 
   registerHealthCheck: 'ws://127.0.0.1:9000?clientType=service',
